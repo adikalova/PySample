ebd94bcb449acb6d3c5061f550452ecf2988a91e
adikalova <adikalova@gmail.com>
2018-11-29 21:22:22 +0200

    added login_test

--- TestLogin.py
+++ TestLogin.py
@@ -1,6 +1,7 @@
 
 
 from selenium import webdriver
+from webdriver_manager import driver
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -9,6 +10,16 @@ def test_login_to_jira():
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get('http://jira.hillel.it:8080/secure/Dashboard.jspa')
     assert "System Dashboard - Hillel IT School JIRA" in driver.title
+
+    driver.find_element_by_id("login-form-username").clear()
+    driver.find_element_by_id("login-form-username").send_keys("adikalova")
+    driver.find_element_by_id("login-form-password").clear()
+    driver.find_element_by_id("login-form-password").send_keys("1234567")
+    driver.find_element_by_id("login").submit()
+
+    assert driver.page_source.find('Dashboard')
+
+
 
 
 
