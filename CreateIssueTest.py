5d18ddaa56f77b286550504123d911b29a006285
adikalova <adikalova@gmail.com>
2018-12-04 21:26:32 +0200

    added UI test for "Create Issue"

--- CreateIssueTest.py
+++ CreateIssueTest.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
-import time
-
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+
 def test_login_to_jira():
     # driver = webdriver.Chrome('C:\\Users\\Анастасия\\Desktop\\python\\PySample\\chromedriver_win32\\chromedriver.exe')
     driver: WebDriver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
     driver.get('http://jira.hillel.it:8080/secure/Dashboard.jspa')
     assert "System Dashboard - Hillel IT School JIRA" in driver.title
 
@@ -20,6 +20,17 @@ def test_login_to_jira():
 
     assert driver.page_source.find('Dashboard')
 
-    time.sleep(3)
+    driver.implicitly_wait(3)
+
     driver.find_element(By.ID, "create_link").click()
-    time.sleep(7)
+    driver.implicitly_wait(7)
+
+    driver.find_element_by_id("summary").clear()
+    driver.find_element_by_id("summary").send_keys("Test26")
+    driver.find_element_by_id("description").send_keys("Text")
+    driver.find_element_by_id("create-issue-submit").click()
+
+    driver.implicitly_wait(7)
+    assert driver.find_element_by_xpath("//*[contains(text(),'Test26')]")
+
+
